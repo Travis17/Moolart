009b8a4f93dbd3f9c9788215b4b7cf85c79f795f
Travis Knoll <trav_k80@yahoo.com>
2013-08-30 12:00:13 -0700

    Created a rails aplication and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moolart::Application.config.secret_token = '6bad5ca49d12aec8cd8301cee4fcf372a6e3ce954b2a2cca29fb0005580b487bb084d5e20a14e7f365e3761ed4f2990abb021488b85fa37b1954058074016a16'
